fae04aab6a1538b6a71555be8c643ae39f6cecde
s185120 <s185120@student.dtu.dk>
2020-03-13 00:15:24 +0100

    ASCII text

--- src/main/java/RunServer.java
+++ src/main/java/RunServer.java
@@ -1,14 +1,34 @@
 import Javalin.Server;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class RunServer {
+    private static DateFormat df = new SimpleDateFormat("[dd-MM-yyyy HH:mm:ss] ");
 
     public static void main(String[] args) {
+        System.out.println(df.format(Calendar.getInstance().getTimeInMillis()) + "Starting server");
+
         Server server = new Server();
         server.setupJavalin();
         server.webUserPaths();
 
         // Initialises the examples of javascript and javalin communication
         server.examples();
+
+        RunServer.printASCII();
+
+        System.out.println(df.format(Calendar.getInstance().getTimeInMillis()) + "Server started");
+    }
+
+    private static void printASCII() {
+        System.out.println("   ____   ____    _   _   ____    ____    _____     _    ___        ");
+        System.out.println("  / ___| |  _ \\  | | | | |  _ \\  |  _ \\  | ____|   / |  / _ \\   ");
+        System.out.println(" | |  _  | |_) | | | | | | |_) | | |_) | |  _|     | | | | | |      ");
+        System.out.println(" | |_| | |  _ <  | |_| | |  __/  |  __/  | |___    | | | |_| |      ");
+        System.out.println("  \\____| |_| \\_\\  \\___/  |_|     |_|     |_____|   |_|  \\___/  ");
+        System.out.println("                                                                    ");
     }
 
 }
